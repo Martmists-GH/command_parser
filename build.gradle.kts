b1faa95a015bdbc6181cfbef31e410c0a4e5e72f
Martmists <martmists@gmail.com>
2022-02-02 11:13:03 +0100

    provide literal names
    Signed-off-by: Martmists <martmists@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.martmists"
-version = "1.2.3"
+version = "1.2.4"
 
 repositories {
     mavenCentral()
--- src/commonMain/kotlin/com/martmists/commandparser/dispatch/LiteralNode.kt
+++ src/commonMain/kotlin/com/martmists/commandparser/dispatch/LiteralNode.kt
@@ -3,6 +3,8 @@ package com.martmists.commandparser.dispatch
 class LiteralNode<C : Context>(names: Array<out String>) : Node<C>() {
     private val sortedNames = names.sortedBy { it.length }.reversed()
 
+    fun getNames() = sortedNames
+
     override suspend fun match(ctx: C, input: String): Pair<Boolean, String> {
         for (name in sortedNames) {
             if (input.startsWith(name)) {
